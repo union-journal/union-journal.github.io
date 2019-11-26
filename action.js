9b708b04ba85cdef4d3de362d3ec79f06b8a8e4b
Ariana Freitag <freitagariana@gmail.com>
2019-11-25 21:08:01 -0500

    added stencil for text

--- action.js
+++ action.js
@@ -2,9 +2,11 @@ $(document).ready(function() {
   $(".icon:nth-child(1)").hover(
     function() {
       $("body").css("background-image", "linear-gradient(#E900E4, #FFFFFF)");
+      $("#info-text").css("background-image", "linear-gradient(#E900E4, #FFFFFF)");
     },
     function() {
       $("body").css("background-image", "linear-gradient(#F90000, #B6D515)");
+      $("#info-text").css("background-image", "linear-gradient(#F90000, #B6D515)");
     }
   );
 
@@ -14,9 +16,14 @@ $(document).ready(function() {
         "background-image",
         "linear-gradient(#000000, #0BAA4B, #000000)"
       );
+      $("#info-text").css(
+        "background-image",
+        "linear-gradient(#000000, #0BAA4B, #000000)"
+      )
     },
     function() {
       $("body").css("background-image", "linear-gradient(#F90000, #B6D515)");
+      $("#info-text").css("background-image", "linear-gradient(#F90000, #B6D515)");
     }
   );
 
@@ -26,45 +33,58 @@ $(document).ready(function() {
         "background-image",
         "linear-gradient(#000000, #43C128, #FFFFFF)"
       );
+      $("#info-text").css(
+        "background-image",
+        "linear-gradient(#000000, #43C128, #FFFFFF)"
+      );
     },
     function() {
       $("body").css("background-image", "linear-gradient(#F90000, #B6D515)");
+      $("#info-text").css("background-image", "linear-gradient(#F90000, #B6D515)");
     }
   );
 
   $(".icon:nth-child(4)").hover(
     function() {
       $("body").css("background-image", "linear-gradient(#000000, #004CBF)");
+      $("#info-text").css("background-image", "linear-gradient(#000000, #004CBF)");
     },
     function() {
       $("body").css("background-image", "linear-gradient(#F90000, #B6D515)");
+      $("#info-text").css("background-image", "linear-gradient(#F90000, #B6D515)");
     }
   );
 
   $(".icon:nth-child(5)").hover(
     function() {
       $("body").css("background-image", "linear-gradient(#001CBF, #FFFFFF)");
+      $("info-text").css("background-image", "linear-gradient(#001CBF, #FFFFFF)");
     },
     function() {
       $("body").css("background-image", "linear-gradient(#F90000, #B6D515)");
+      $("#info-text").css("background-image", "linear-gradient(#F90000, #B6D515)");
     }
   );
 
   $(".icon:nth-child(6)").hover(
     function() {
       $("body").css("background-image", "linear-gradient(#000000, #D40000)");
+      $("#info-text").css("background-image", "linear-gradient(#000000, #D40000)");
     },
     function() {
       $("body").css("background-image", "linear-gradient(#F90000, #B6D515)");
+      $("#info-text").css("background-image", "linear-gradient(#F90000, #B6D515)");
     }
   );
 
   $(".icon:nth-child(7)").hover(
     function() {
       $("body").css("background-image", "linear-gradient(#00EE00, #EFEFEF)");
+      $("#info-text").css("background-image", "linear-gradient(#00EE00, #EFEFEF)");
     },
     function() {
       $("body").css("background-image", "linear-gradient(#F90000, #B6D515)");
+      $("#info-text").css("background-image", "linear-gradient(#F90000, #B6D515)");
     }
   );
 
@@ -74,9 +94,14 @@ $(document).ready(function() {
         "background-image",
         "linear-gradient(#FFFFFF, #F9CA09, #FFFFFF)"
       );
+      $("#info-text").css(
+        "background-image",
+        "linear-gradient(#FFFFFF, #F9CA09, #FFFFFF)"
+      );
     },
     function() {
       $("body").css("background-image", "linear-gradient(#F90000, #B6D515)");
+      $("#info-text").css("background-image", "linear-gradient(#F90000, #B6D515)");
     }
   );
 });
